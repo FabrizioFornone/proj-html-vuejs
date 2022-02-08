007944808f390bb2c93c1ae2f98e2d3f5eaeb4f8
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-08 15:51:11 +0100

    Second Section Done

--- src/components/SecondSectionBox.vue
+++ src/components/SecondSectionBox.vue
@@ -1,16 +1,61 @@
 <template>
-<section>
-</section>
-    
+  <section>
+    <div class="container pt-5 my-3">
+      <div class="text-center">
+        <h5 class="text-uppercase">phasellus eget metus</h5>
+        <h2 class="fw-bold py-2">All the latest news</h2>
+        <hr />
+      </div>
+      <div class="row my-5">
+        <div class="col-4">
+          <img class="img-fluid" src="@/assets/img/smartphone1.jpg" alt="" />
+          <div class="fw-bold pt-1">
+            Morbi vitae dui euismod vuluptate sollicitudin
+          </div>
+          <div class="subtitle pt-1">
+            <span> October 11th, 205 | </span> <strong>2 comments</strong>
+            <p class="py-2">
+              Donec finibus sit aemet orci eget ultricies. Praesent posuere ante
+              ut erat fringilla, stibulum placerat metus mattis. Aenean dictum
+              vitae nisl
+            </p>
+          </div>
+        </div>
+        <div class="col-4">
+          <img class="img-fluid" src="@/assets/img/smartphone2.jpg" alt="" />
+          <div class="fw-bold pt-1">Vivamus pellenteque, felis quis varius</div>
+          <div class="subtitle pt-1">
+            <span> October 11th, 205 | </span> <span>Comments Off</span>
+            <p class="py-2">
+              Donec finibus sit aemet orci eget ultricies. Praesent posuere ante
+              ut erat fringilla, stibulum placerat metus mattis. Aenean dictum
+              vitae nisl
+            </p>
+          </div>
+        </div>
+        <div class="col-4">
+          <img class="img-fluid" src="@/assets/img/smartwatch.jpg" alt="" />
+          <div class="fw-bold pt-1">
+            Donec ornare pretium eget scelisque justo
+          </div>
+          <div class="subtitle pt-1">
+            <span> October 11th, 205 | </span> <span>Comments Off</span>
+            <p class="py-2">
+              Donec finibus sit aemet orci eget ultricies. Praesent posuere ante
+              ut erat fringilla, stibulum placerat metus mattis. Aenean dictum
+              vitae nisl
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
 @import "@/style/secondSectionStyle.scss";
-
 </style>
