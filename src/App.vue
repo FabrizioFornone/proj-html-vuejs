5af8e9d511f4302b36f2b6c0823c513b127be3c8
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-08 11:11:14 +0100

    HeaderBox.vue component created + headerStyle.scss

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
-    <h1 class="text-secondary">test</h1>
+    <header-box />
   </div>
 </template>
 
 <script>
+import HeaderBox from "./components/HeaderBox.vue";
 export default {
   name: "App",
-  components: {},
+  components: { HeaderBox },
 };
 </script>
 
