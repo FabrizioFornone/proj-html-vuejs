f5747bb8852b08945e9e81e88ffc35c7a565d7bc
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-11 11:03:45 +0100

    small fixes

--- src/components/HeadingTextBox.vue
+++ src/components/HeadingTextBox.vue
@@ -3,7 +3,7 @@
     <div class="container py-5">
       <div class="text-center py-5">
         <h5 class="text-uppercase">avada forum is the place to be</h5>
-        <p class="py-1">
+        <p class="py-1 my-0 mx-auto">
           In quis lectus sed leo elementum facibus in dapibus dictum. Nulla
           molestie tortor nec lectus venenatis, sed blandit dui finibus. Dolor
           at, bibendum purus. Donec vehicula nec tortor ac finibus.
